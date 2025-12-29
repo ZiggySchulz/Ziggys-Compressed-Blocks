3c2b8e7ca68e9e55f943afcd4cb1e623a3567893
Ziggy Schulz <ziggyschulz@gmail.com>
2025-12-30 10:01:53 +1100

    Initial commit

---
+++ src/client/java/ziggy/ultimatecompressedblocks/UltimateCompressedBlocksClient.java
@@ -0,0 +1,10 @@
+package ziggy.ultimatecompressedblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltimateCompressedBlocksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
