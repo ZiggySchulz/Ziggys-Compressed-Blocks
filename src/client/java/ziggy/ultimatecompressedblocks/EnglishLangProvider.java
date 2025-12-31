a504141b7bd4d2f986ec076372818d0d31dde94d
Ziggy Schulz <ziggyschulz@gmail.com>
2025-12-31 20:30:30 +1100

    Auto Lang Generation

---
+++ src/client/java/ziggy/ultimatecompressedblocks/EnglishLangProvider.java
@@ -0,0 +1,35 @@
+package ziggy.ultimatecompressedblocks;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider.TranslationBuilder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Item;
+
+public class EnglishLangProvider extends FabricLanguageProvider {
+    protected EnglishLangProvider(FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
+		// Specifying en_us is optional, as it's the default language code
+		super(dataOutput, "en_us", registryLookup);
+	}
+
+	@Override
+	public void generateTranslations(HolderLookup.Provider wrapperLookup, TranslationBuilder translationBuilder) {
+        for (Item item : ModBlocks.SupportedItems) {
+            String itemName = BuiltInRegistries.ITEM.getKey(item).getPath();
+            itemName = "Compressed " + toTitleCase(itemName.replace('_', ' '));
+            translationBuilder.add("item.ultimate_compressed_blocks.compressed_" + BuiltInRegistries.ITEM.getKey(item).getPath(), itemName);
+        }
+	}
+
+
+    private String toTitleCase(String input) {
+        return Arrays.stream(input.toLowerCase().split(" "))
+                .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1))
+                .collect(Collectors.joining(" "));
+    }
+}
