a8871ee94e68627a78e45dcdac5b1a46511fc0b4
Ziggy Schulz <ziggyschulz@gmail.com>
2026-01-07 16:50:13 +1100

    Formating change

--- src/main/java/ziggy/ziggyscompressedblocks/ModBlocks.java
+++ src/main/java/ziggy/ziggyscompressedblocks/ModBlocks.java
@@ -25,38 +25,170 @@ public class ModBlocks {
     // Supported blocks. Add new supported blocks here
     public static final SupportedItemInfo[] SupportedItems = {
         // World generation blocks
-        new SupportedItemInfo(Items.DIRT, Blocks.DIRT, List.of(BlockTags.MINEABLE_WITH_SHOVEL)),
-        new SupportedItemInfo(Items.COBBLESTONE, Blocks.COBBLESTONE, List.of(BlockTags.MINEABLE_WITH_PICKAXE)),
-        new SupportedItemInfo(Items.STONE, Blocks.STONE, List.of(BlockTags.MINEABLE_WITH_PICKAXE)),
-        new SupportedItemInfo(Items.SAND, Blocks.SAND, List.of(BlockTags.MINEABLE_WITH_SHOVEL)),
-        new SupportedItemInfo(Items.GRASS_BLOCK, Blocks.GRASS_BLOCK, List.of(BlockTags.MINEABLE_WITH_SHOVEL)),
-        new SupportedItemInfo(Items.NETHERRACK, Blocks.NETHERRACK, List.of(BlockTags.MINEABLE_WITH_PICKAXE, BlockTags.INFINIBURN_OVERWORLD, BlockTags.INFINIBURN_NETHER, BlockTags.INFINIBURN_END)),
-        new SupportedItemInfo(Items.END_STONE, Blocks.END_STONE, List.of(BlockTags.MINEABLE_WITH_PICKAXE)),
-        new SupportedItemInfo(Items.OBSIDIAN, Blocks.OBSIDIAN, List.of(BlockTags.MINEABLE_WITH_PICKAXE, BlockTags.NEEDS_DIAMOND_TOOL)),
-        new SupportedItemInfo(Items.COBBLED_DEEPSLATE, Blocks.COBBLED_DEEPSLATE, List.of(BlockTags.MINEABLE_WITH_PICKAXE)),
-        new SupportedItemInfo(Items.DEEPSLATE, Blocks.DEEPSLATE, List.of(BlockTags.MINEABLE_WITH_PICKAXE)),
-        new SupportedItemInfo(Items.BLACKSTONE, Blocks.BLACKSTONE, List.of(BlockTags.MINEABLE_WITH_PICKAXE)),
-        new SupportedItemInfo(Items.BASALT, Blocks.BASALT, List.of(BlockTags.MINEABLE_WITH_PICKAXE)),
-        new SupportedItemInfo(Items.MOSS_BLOCK, Blocks.MOSS_BLOCK, List.of(BlockTags.MINEABLE_WITH_HOE)),
+        new SupportedItemInfo(
+            Items.DIRT, 
+            Blocks.DIRT, 
+            List.of(BlockTags.MINEABLE_WITH_SHOVEL)
+        ),
+        new SupportedItemInfo(
+            Items.COBBLESTONE, 
+            Blocks.COBBLESTONE, 
+            List.of(BlockTags.MINEABLE_WITH_PICKAXE)
+        ),
+        new SupportedItemInfo(
+            Items.STONE, 
+            Blocks.STONE, 
+            List.of(BlockTags.MINEABLE_WITH_PICKAXE)
+        ),
+        new SupportedItemInfo(
+            Items.SAND, 
+            Blocks.SAND, 
+            List.of(BlockTags.MINEABLE_WITH_SHOVEL)
+        ),
+        new SupportedItemInfo(
+            Items.GRASS_BLOCK, 
+            Blocks.GRASS_BLOCK, 
+            List.of(BlockTags.MINEABLE_WITH_SHOVEL)
+        ),
+        new SupportedItemInfo(
+            Items.NETHERRACK, 
+            Blocks.NETHERRACK,
+            List.of(
+                BlockTags.MINEABLE_WITH_PICKAXE, 
+                BlockTags.INFINIBURN_OVERWORLD,
+                BlockTags.INFINIBURN_NETHER, 
+                BlockTags.INFINIBURN_END
+            )
+        ),
+        new SupportedItemInfo(
+            Items.END_STONE, 
+            Blocks.END_STONE, 
+            List.of(BlockTags.MINEABLE_WITH_PICKAXE)
+        ),
+        new SupportedItemInfo(
+            Items.OBSIDIAN, 
+            Blocks.OBSIDIAN, 
+            List.of(
+                BlockTags.MINEABLE_WITH_PICKAXE, 
+                BlockTags.NEEDS_DIAMOND_TOOL
+            )
+        ),
+        new SupportedItemInfo(
+            Items.COBBLED_DEEPSLATE, 
+            Blocks.COBBLED_DEEPSLATE, 
+            List.of(BlockTags.MINEABLE_WITH_PICKAXE)
+        ),
+        new SupportedItemInfo(
+            Items.DEEPSLATE, 
+            Blocks.DEEPSLATE, 
+            List.of(BlockTags.MINEABLE_WITH_PICKAXE)
+        ),
+        new SupportedItemInfo(
+            Items.BLACKSTONE, 
+            Blocks.BLACKSTONE, 
+            List.of(BlockTags.MINEABLE_WITH_PICKAXE)
+        ),
+        new SupportedItemInfo(
+            Items.BASALT, 
+            Blocks.BASALT, 
+            List.of(BlockTags.MINEABLE_WITH_PICKAXE)
+        ),
+        new SupportedItemInfo(
+            Items.MOSS_BLOCK, 
+            Blocks.MOSS_BLOCK, 
+            List.of(BlockTags.MINEABLE_WITH_HOE)
+        ),
 
         // Crops
-        new SupportedItemInfo(Items.CARROT, 0.3f, SoundType.CROP, List.of(BlockTags.MINEABLE_WITH_HOE)),
-        new SupportedItemInfo(Items.POTATO, 0.3f, SoundType.CROP, List.of(BlockTags.MINEABLE_WITH_HOE)),
-        new SupportedItemInfo(Items.BEETROOT, 0.3f, SoundType.CROP, List.of(BlockTags.MINEABLE_WITH_HOE)),
-        new SupportedItemInfo(Items.SUGAR_CANE, 0.25f, SoundType.BAMBOO, List.of(BlockTags.MINEABLE_WITH_HOE)),
-        new SupportedItemInfo(Items.MELON, Blocks.MELON, List.of(BlockTags.MINEABLE_WITH_AXE)),
-        new SupportedItemInfo(Items.PUMPKIN, Blocks.PUMPKIN, List.of(BlockTags.MINEABLE_WITH_AXE)),
-        new SupportedItemInfo(Items.CACTUS, Blocks.CACTUS, List.of(BlockTags.MINEABLE_WITH_SHOVEL)),
+        new SupportedItemInfo(
+            Items.CARROT, 
+            0.3f, 
+            SoundType.CROP, 
+            List.of(BlockTags.MINEABLE_WITH_HOE)
+        ),
+        new SupportedItemInfo(
+            Items.POTATO, 
+            0.3f, 
+            SoundType.CROP, 
+            List.of(BlockTags.MINEABLE_WITH_HOE)
+        ),
+        new SupportedItemInfo(
+            Items.BEETROOT, 
+            0.3f, 
+            SoundType.CROP, 
+            List.of(BlockTags.MINEABLE_WITH_HOE)
+        ),
+        new SupportedItemInfo(
+            Items.SUGAR_CANE, 
+            0.25f, 
+            SoundType.BAMBOO, 
+            List.of(BlockTags.MINEABLE_WITH_HOE)
+        ),
+        new SupportedItemInfo(
+            Items.MELON, 
+            Blocks.MELON, 
+            List.of(BlockTags.MINEABLE_WITH_AXE)
+        ),
+        new SupportedItemInfo(
+            Items.PUMPKIN, 
+            Blocks.PUMPKIN, 
+            List.of(BlockTags.MINEABLE_WITH_AXE)
+        ),
+        new SupportedItemInfo(
+            Items.CACTUS, 
+            Blocks.CACTUS, 
+            List.of(BlockTags.MINEABLE_WITH_SHOVEL)
+        ),
 
         // Mobs
-        new SupportedItemInfo(Items.FEATHER, 0.1f, SoundType.WOOL, List.of(BlockTags.SWORD_EFFICIENT)),
-        new SupportedItemInfo(Items.EGG, 0.1f, SoundType.MUD_BRICKS, List.of(BlockTags.MINEABLE_WITH_PICKAXE)),
-        new SupportedItemInfo(Items.LEATHER, 0.2f, SoundType.WOOL, List.of(BlockTags.SWORD_EFFICIENT)),
-        new SupportedItemInfo(Items.ARROW, 0.1f, SoundType.WOOD, List.of(BlockTags.MINEABLE_WITH_AXE)),
-        new SupportedItemInfo(Items.SPIDER_EYE, 0.1f, SoundType.NETHER_WART, List.of(BlockTags.SWORD_EFFICIENT)),
-        new SupportedItemInfo(Items.GUNPOWDER, 0.1f, SoundType.SAND, List.of(BlockTags.MINEABLE_WITH_SHOVEL)),
-        new SupportedItemInfo(Items.ROTTEN_FLESH, 0.1f, SoundType.NETHER_WART, List.of(BlockTags.SWORD_EFFICIENT)),
-        new SupportedItemInfo(Items.GHAST_TEAR, 0.3f, SoundType.DRIED_GHAST, List.of(BlockTags.MINEABLE_WITH_PICKAXE)),
+        new SupportedItemInfo(
+            Items.FEATHER, 
+            0.1f, 
+            SoundType.WOOL, 
+            List.of(BlockTags.SWORD_EFFICIENT)
+        ),
+        new SupportedItemInfo(
+            Items.EGG, 
+            0.1f, 
+            SoundType.MUD_BRICKS, 
+            List.of(BlockTags.MINEABLE_WITH_PICKAXE)
+        ),
+        new SupportedItemInfo(
+            Items.LEATHER, 
+            0.2f, 
+            SoundType.WOOL, 
+            List.of(BlockTags.SWORD_EFFICIENT)
+        ),
+        new SupportedItemInfo(
+            Items.ARROW, 
+            0.1f, 
+            SoundType.WOOD, 
+            List.of(BlockTags.MINEABLE_WITH_AXE)
+        ),
+        new SupportedItemInfo(
+            Items.SPIDER_EYE, 
+            0.1f, 
+            SoundType.NETHER_WART, 
+            List.of(BlockTags.SWORD_EFFICIENT)
+        ),
+        new SupportedItemInfo(
+            Items.GUNPOWDER, 
+            0.1f, 
+            SoundType.SAND, 
+            List.of(BlockTags.MINEABLE_WITH_SHOVEL)
+        ),
+        new SupportedItemInfo(
+            Items.ROTTEN_FLESH, 
+            0.1f, 
+            SoundType.NETHER_WART, 
+            List.of(BlockTags.SWORD_EFFICIENT)
+        ),
+        new SupportedItemInfo(
+            Items.GHAST_TEAR, 
+            0.3f, 
+            SoundType.DRIED_GHAST, 
+            List.of(BlockTags.MINEABLE_WITH_PICKAXE)
+        ),
         
         new SupportedItemInfo(
             Items.ENDER_PEARL,
